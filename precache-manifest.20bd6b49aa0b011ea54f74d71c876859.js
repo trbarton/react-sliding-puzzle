5c74e230f4bcf81a70c5e94042aa5e11e5020c14
Tom Barton <tbarty95@gmail.com>
2018-12-29 14:58:00 +0100

    Updates

---
+++ precache-manifest.20bd6b49aa0b011ea54f74d71c876859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6030456b7409f7025749",
+    "url": "/react-sliding-puzzle/static/js/runtime~main.6030456b.js"
+  },
+  {
+    "revision": "ff785acc920637b16214",
+    "url": "/react-sliding-puzzle/static/js/main.ff785acc.chunk.js"
+  },
+  {
+    "revision": "07140812a98c93f90dd2",
+    "url": "/react-sliding-puzzle/static/js/1.07140812.chunk.js"
+  },
+  {
+    "revision": "ff785acc920637b16214",
+    "url": "/react-sliding-puzzle/static/css/main.4245c42b.chunk.css"
+  },
+  {
+    "revision": "e998abc9d4075debf58498a5f1d2f2e4",
+    "url": "/react-sliding-puzzle/index.html"
+  }
+];
