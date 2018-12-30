f4f99cb6b8703a99f3a477151c550f4df7c2bb1c
Tom Barton <tbarty95@gmail.com>
2018-12-30 15:17:56 +0100

    Updates

---
+++ precache-manifest.1dae62d91045306635749fe6c18ece84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6030456b7409f7025749",
+    "url": "/react-sliding-puzzle/static/js/runtime~main.6030456b.js"
+  },
+  {
+    "revision": "356b4a39f242be3c5a52",
+    "url": "/react-sliding-puzzle/static/js/main.356b4a39.chunk.js"
+  },
+  {
+    "revision": "c9c69a0819e9d8f97194",
+    "url": "/react-sliding-puzzle/static/js/1.c9c69a08.chunk.js"
+  },
+  {
+    "revision": "356b4a39f242be3c5a52",
+    "url": "/react-sliding-puzzle/static/css/main.f851e6fd.chunk.css"
+  },
+  {
+    "revision": "96a10db714fd47211ee237cf08915d82",
+    "url": "/react-sliding-puzzle/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-sliding-puzzle/precache-manifest.9c13ab55f8fc76104603ec963b851441.js"
+  "/react-sliding-puzzle/precache-manifest.1dae62d91045306635749fe6c18ece84.js"
 );
 
 workbox.clientsClaim();
